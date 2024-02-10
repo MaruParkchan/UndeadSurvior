1620fd1cbbddcecfd58087502e2fec77c48cf7bb
ColdPark <ekzm103@gmail.com>
2024-02-10 20:23:00 +0900

    플레이어 애니메이터 설정
    1, 애니메이션 Player 0 , Player 1 2가지 만듬
    2, 애니메이터 오버라이드 1개 추가후 Player1 추가
    3, Flip을 Vector값을 이용하여 반전 사용

--- Assets/#Script/Player.cs
+++ Assets/#Script/Player.cs
@@ -3,22 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-    private Rigidbody2D playerRigidbody;
     [SerializeField] private Vector2 inputVector;
+    private Rigidbody2D playerRigidbody;
+    private SpriteRenderer playerSpriteRender;
+    private Animator playerAnimator;
 
-    private void Start()
+    private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRender = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
     }
 
-    private void FixedUpdate() 
-    {    
+    private void FixedUpdate()
+    {
         Vector2 nextVector = inputVector.normalized * moveSpeed * Time.fixedDeltaTime; // FixedDeltaTime은 FixedUpdate(물리 프레임마다)에 써야한다
         playerRigidbody.MovePosition(playerRigidbody.position + nextVector);
+    }
+
+    private void LateUpdate()
+    {
+        PlayerFlipReversing();
+        AnimationChange();
+    }
+
+    void PlayerFlipReversing() // inputVector에 따른 Flip 반전 
+    {
+        if (inputVector.x != 0)
+        {
+            playerSpriteRender.flipX = inputVector.x < 0; // inputVector.x 가 0보다 작으면 true 
+        }
+    }
+
+    void AnimationChange()
+    {
+        playerAnimator.SetFloat("Speed", inputVector.magnitude);
     }
 
     void OnMove(InputValue value)
