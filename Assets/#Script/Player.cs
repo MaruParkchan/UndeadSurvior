efe77423883438d9a644d8b482dd9e59399f28c7
ColdPark <ekzm103@gmail.com>
2024-02-10 17:54:15 +0900

    플레이어 기본 이동구현

---
+++ Assets/#Script/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.0f;
+    private Rigidbody2D playerRigidbody;
+    private Vector2 inputVector;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate() 
+    {    
+        Vector2 nextVector = inputVector.normalized * moveSpeed * Time.fixedDeltaTime; // FixedDeltaTime은 FixedUpdate(물리 프레임마다)에 써야한다
+        playerRigidbody.MovePosition(playerRigidbody.position + nextVector);
+    }
+}
