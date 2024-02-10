c668f5c44dcbeefea8734bbf84e3c00bf491b646
ColdPark <ekzm103@gmail.com>
2024-02-10 18:09:23 +0900

    유니티 Input System 적용
    기존 이동 하는 구현 말고 유니티에서 InputSystem 을 새로 도입하여 사용

--- Assets/#Script/Player.cs
+++ Assets/#Script/Player.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
     private Rigidbody2D playerRigidbody;
-    private Vector2 inputVector;
+    [SerializeField] private Vector2 inputVector;
 
     private void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        inputVector.x = Input.GetAxisRaw("Horizontal");
-        inputVector.y = Input.GetAxisRaw("Vertical");
-    }
-
     private void FixedUpdate() 
     {    
         Vector2 nextVector = inputVector.normalized * moveSpeed * Time.fixedDeltaTime; // FixedDeltaTime은 FixedUpdate(물리 프레임마다)에 써야한다
         playerRigidbody.MovePosition(playerRigidbody.position + nextVector);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVector = value.Get<Vector2>();
     }
 }
