38bd8dbc1225dea7ad90a71afb8bbefb4805903e
ColdPark <ekzm103@gmail.com>
2024-02-14 23:32:45 +0900

    총알 Per 관통력 수정
    1, -1이 관통이 아닌 -100으로 설정

--- Assets/#Script/Bullet.cs
+++ Assets/#Script/Bullet.cs
@@ -13,7 +13,7 @@ public class Bullet : MonoBehaviour
         this.damage = damage;
         this.per = per;
 
-        if(per > -1)
+        if(per >= 0)
         {
             rigid.velocity = dir * 15f;
         }
@@ -26,12 +26,12 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(!other.CompareTag("Enemy") || per == -1)
+        if(!other.CompareTag("Enemy") || per == -100)
             return;
 
         per--; // 관통을 했으면 per가 줄어듬 
 
-        if(per == -1)
+        if(per < 0)
         {   
             rigid.velocity = Vector2.zero;
             gameObject.SetActive(false);
