0fa0655a9e995088b9f96b0a70c4c4f4445ad682
ColdPark <ekzm103@gmail.com>
2024-02-14 23:30:38 +0900

    리팩토링 - 배경, 적 오브젝트 코드설정
    배경 - 입력값에 따른 배경 위치 이동을 수정함 (입력값 없앰)
    
    적 - 일정 위치에 플레이어가 벗어나면 다시 재배치

--- Assets/#Script/Reposition.cs
+++ Assets/#Script/Reposition.cs
@@ -12,38 +12,40 @@ public class Reposition : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(!other.CompareTag("Area")) // Area 태그가 아니면
+        if (!other.CompareTag("Area")) // Area 태그가 아니면
             return;
 
-        Vector2 playerPosition = GameManager.instance.player.transform.position;
+        Vector3 playerPosition = GameManager.instance.player.transform.position;
         Vector3 myPosition = transform.position;
-        float diffX = Mathf.Abs(playerPosition.x - myPosition.x);
-        float diffY = Mathf.Abs(playerPosition.y - myPosition.y);
 
-        Vector3 playerDirection = GameManager.instance.player.inputVector;
-        float directionX = playerDirection.x < 0 ? -1 : 1;
-        float directionY = playerDirection.y < 0 ? -1 : 1;
-
-        switch(transform.tag)
+        switch (transform.tag)
         {
             case "Ground":
-                if(diffX > diffY) // x축 이동
+                float diffX = Mathf.Abs(playerPosition.x - myPosition.x);
+                float diffY = Mathf.Abs(playerPosition.y - myPosition.y);
+                float directionX = diffX < 0 ? -1 : 1;
+                float directionY = diffY < 0 ? -1 : 1;
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+                if (diffX > diffY) // x축 이동
                 {
                     transform.Translate(Vector3.right * directionX * 40); // 2칸 가기 때문에 40 
-                } 
-                else if(diffX <diffY)
+                }
+                else if (diffX < diffY)
                 {
                     transform.Translate(Vector3.up * directionY * 40);
                 }
-            break;
+                break;
 
             case "Enemy":
-            if(coll.enabled)
-            {
-                transform.Translate(playerDirection * 20 + new Vector3(Random.Range(-3f,3f), Random.Range(-3f,3f), 0));
-            }
+                if (coll.enabled)
+                {   
+                   Vector3 dist = playerPosition - myPosition;
+                   Vector3 ran = new Vector3(Random.Range(-3, 3),Random.Range(-3, 3),0);
+                    transform.Translate(ran + dist * 2 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                }
 
-            break;
+                break;
         }
     }
 }
